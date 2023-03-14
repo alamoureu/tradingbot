5a75b4728aa000b08382c5e633b90cae4be135ba
AntoineLamoureux-crypto <70601428+AntoineLamoureux-crypto@users.noreply.github.com>
2023-03-14 17:06:22 -0400

    push

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, abort
-import telegram_send
+from pymongo import MongoClient
 import requests
 
 app = Flask(__name__)
@@ -10,18 +10,22 @@ def send_alert(data):
     chatID = data["telegram"].encode("latin-1", "backslashreplace").decode("unicode_escape")
     if key == "asd12dsdf13-a":
         try:
-            requests.post('https://api.telegram.org/bot5074796706:AAGbPAthMi0ZIty0ioc2j_31WvrvNQs8MQU/sendMessage?chat_id=' + str(chatID) + '&text=' + msg)
+            requests.post('https://api.telegram.org/bot5916087740:AAH9z84E0qVRyZC6q5sg9BdsLPxPKuPjHVQ/sendMessage?chat_id=' + str(chatID) + '&text=' + msg)
         except KeyError:
-            requests.post('https://api.telegram.org/bot5074796706:AAGbPAthMi0ZIty0ioc2j_31WvrvNQs8MQU/sendMessage?chat_id=-776291763&text=ERORR')
+            requests.post('https://api.telegram.org/bot5916087740:AAH9z84E0qVRyZC6q5sg9BdsLPxPKuPjHVQ/sendMessage?chat_id=-1819896075&text=ERORR')
         except Exception as e:
             print("[X] Telegram Error:\n>", e)
 
-@app.route('/webhook', methods=['POST'])
+@app.route('/notify', methods=['POST'])
 def webhook():
+    myclient = MongoClient("mongodb+srv://Antoine:123@cluster0.yraw2.mongodb.net/?retryWrites=true&w=majority")
+    db = myclient["fx-trading"]
+    options = db["Options"]
+    
     if request.method == 'POST':
-        print(request.json)
         data = request.get_json()
-        send_alert(data)
+        send_alert(data) 
+        options.insert_one({"data" : data})
         return 'success', 200
     else:
         abort(400)
--- webhook.py
+++ webhook.py
@@ -1,10 +1,8 @@
 import requests
 import json
 
-webhook_url = "http://192.168.2.51:80/webhook"
+webhook_url = "https://davidprojects.pythonanywhere.com/notify"
 
-data =[{"telegram": "-680210585", "msg": "Allo Antoine CHAT 1", "key": "asd12dsdf13-a"},{"telegram": "-773242778", "msg": "Allo Antoine CHAT 2", "key": "asd12dsdf13-a"}]
+data = {"telegram": "-1001819896075", "msg": "BUY AAPL", "key": "asd12dsdf13-a"}
 
-r1 = requests.post(webhook_url, data = json.dumps(data[0]), headers = {'Content-type' : 'application/json'})
-
-#r2 = requests.post(webhook_url, data = json.dumps(data[1]), headers = {'Content-type' : 'application/json'})
+r1 = requests.post(webhook_url, data = json.dumps(data), headers = {'Content-type' : 'application/json'})
